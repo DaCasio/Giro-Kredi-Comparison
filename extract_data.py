2c1c96f24459313b92e2df9c51f258632d773c8a
DaCasio <140727178+DaCasio@users.noreply.github.com>
2025-02-09 15:11:21 +0100

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -1,19 +1,10 @@
 import pandas as pd
 
 def extract_data():
-    # URL des öffentlichen Google Sheets-Dokuments
     url = "https://docs.google.com/spreadsheets/d/1syH5ntimv_5juHGOZo0LUgLO1Jk2kEQjhno8Kl21jzw/export?format=csv&gid=0"
-
-    # Daten in ein DataFrame laden
     data = pd.read_csv(url)
-
-    # Monatsnamen und Kontostände extrahieren
-    monthly_names = data.iloc[0, :].values  # A1 bis BY1
-    monthly_balances = data.iloc[1, :].values  # A2 bis BY2
-
-    return monthly_names, monthly_balances
+    return data.iloc[0, :].values, data.iloc[1, :].values  # Monatsnamen, Kontostände
 
 if __name__ == "__main__":
-    monthly_names, monthly_balances = extract_data()
-    print("Monatsnamen:", monthly_names)
-    print("Kontostände:", monthly_balances)
+    months, balances = extract_data()
+    print(f"Monate: {months}\nKontostände: {balances}")
