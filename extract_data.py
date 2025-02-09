394f73a58af59d998c18d3677486f48926c6a07f
DaCasio <140727178+DaCasio@users.noreply.github.com>
2025-02-09 15:22:48 +0100

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -3,8 +3,4 @@ import pandas as pd
 def extract_data():
     url = "https://docs.google.com/spreadsheets/d/1syH5ntimv_5juHGOZo0LUgLO1Jk2kEQjhno8Kl21jzw/export?format=csv&gid=0"
     data = pd.read_csv(url)
-    return data.iloc[0, :].values, data.iloc[1, :].values  # Monatsnamen, Kontostände
-
-if __name__ == "__main__":
-    months, balances = extract_data()
-    print(f"Monate: {months}\nKontostände: {balances}")
+    return data.iloc[0, :].values, data.iloc[1, :].values
