bba6fa53cb160da5c0b28622af33e50387ae5c90
DaCasio <140727178+DaCasio@users.noreply.github.com>
2025-02-10 20:52:59 +0100

    Update compare_balances.py

--- compare_balances.py
+++ compare_balances.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         "frames": [
             {
                 "text": f"{result}",
-                "icon": "i616"  # Euro-Symbol
+                "icon": "544"  # Done Check
             }
         ]
     }
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         "frames": [
             {
                 "text": f"{int(summe)}€",
-                "icon": "i616"  # Euro-Symbol
+                "icon": "3769"  # Arrow
             }
         ]
     }
