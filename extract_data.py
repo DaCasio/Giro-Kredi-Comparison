e7d3d8c03b99f880291661da35b33b9ab19f12c0
DaCasio <140727178+DaCasio@users.noreply.github.com>
2025-02-16 18:28:02 +0100

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def extract_data():
-    # URL für den Tab "dat"
     url = "https://docs.google.com/spreadsheets/d/1syH5ntimv_5juHGOZo0LUgLO1Jk2kEQjhno8Kl21jzw/export?format=csv&gid=0"
     try:
         # CSV-Daten laden
@@ -14,7 +13,7 @@ def extract_data():
         if data.shape[0] < 2:
             raise ValueError("Google Sheet hat weniger als 2 Zeilen")
         
-        # Extrahieren der Monatsnamen und Kontostände
+        # Extrahieren der Monatsnamen und Kontostände aus Zeile 1 und 2
         months = data.iloc[0, :].values  # Zeile 1: Monatsnamen
         balances = [
             float(value.replace(".", "").replace(",", ".").strip())  # Tausendertrennzeichen entfernen, Dezimalpunkt setzen
